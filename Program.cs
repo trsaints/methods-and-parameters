f5195aed9beec46bae567c8e48fa44f48179d22c
trsaints <thiago.dev2k3@gmail.com>
2024-03-23 22:49:29 -0300

    add first method with parameters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] schedule = { 800, 1200, 1600, 2000 };
+
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+{
+    int diff = 0;
+
+    bool isInvalidGMT = Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12;
+
+    if (isInvalidGMT)
+        throw new Exception("Invalid GMT");
+
+    if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    else
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+    for (int i = 0; i < times.Length; i++)
+    {
+        int newTime = (times[i] + diff) % 2400;
+        Console.WriteLine($"{times[i]} -> {newTime}");
+    }
+}
+
+DisplayAdjustedTimes(schedule, 6, -6);
